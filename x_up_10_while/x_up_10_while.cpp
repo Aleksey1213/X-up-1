d107534cb12d84e6a690e564665750c26249a345
vsudov <vanua.dubinkin@gmail.com>
2022-10-17 23:10:57 +0300

    Add files

---
+++ x_up_10_while/x_up_10_while.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    int n, S, i, x;
+    S = 0;
+
+    cout << "Введите количество чисел:";
+    cin >> n;
+
+    i = 0;
+
+    while (i < n)
+    {
+        cout << "Введите число:";
+        cin >> x;
+
+        if (x > 10) {
+            S = S + x;
+            i += 1;
+        }
+    }
+
+    cout << "Сумма чисел, больших 10:" << S << endl;
+
+    return 0;
+}
